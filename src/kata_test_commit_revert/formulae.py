ac13f002bba28008b2398e86e23c9b1d270813ff
Rajeev Karamcheti <rkaramc@gmail.com>
2026-01-01 01:06:37 +0530

    fibonacci works for [1,3]

---
+++ src/kata_test_commit_revert/formulae.py
@@ -0,0 +1,4 @@
+def fibonacci(n: int) -> int:
+    if n == 1:
+        return 1
+    return n - 1
--- tests/test_fibonacci.py
+++ tests/test_fibonacci.py
@@ -1,6 +1,15 @@
+from kata_test_commit_revert.formulae import fibonacci
+
+
 class TestFibonacci:
     def test_fibonacci(self):
         assert True
 
+    def test_fibonacci_1(self):
+        assert fibonacci(1) == 1
+
     def test_fibonacci_2(self):
-        assert True
+        assert fibonacci(2) == 1
+
+    def test_fibonacci_3(self):
+        assert fibonacci(3) == 2
