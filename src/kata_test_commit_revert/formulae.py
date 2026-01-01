ccd187c04cc566be4fdbd088605c3620aa0af454
Rajeev Karamcheti <rkaramc@gmail.com>
2026-01-01 11:05:25 +0530

    refactor: simplify conditions

--- src/kata_test_commit_revert/formulae.py
+++ src/kata_test_commit_revert/formulae.py
@@ -5,10 +5,6 @@ import functools
 def fibonacci(n: int) -> int:
     if n < 0:
         raise ValueError("n must be non-negative")
-    if n == 0:
-        return 0
-    if n == 1:
-        return 1
-    if n > 4:
-        return fibonacci(n - 1) + fibonacci(n - 2)
-    return n - 1
+    if n < 2:
+        return n
+    return fibonacci(n - 1) + fibonacci(n - 2)
